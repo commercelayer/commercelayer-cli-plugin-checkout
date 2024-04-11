af8e672a766af063c3956233c18aec6203690d1c
Pierluigi Viti <pierluigi@commercelayer.io>
2024-04-11 15:43:02 +0200

    fix: replace ts-node with tsx

--- bin/dev.js
+++ bin/dev.js
@@ -1,5 +1,5 @@
-#!/usr/bin/env node_modules/.bin/ts-node
-// eslint-disable-next-line node/shebang, unicorn/prefer-top-level-await
+#!/usr/bin/env node_modules/.bin/tsx
+
 ;(async () => {
   const oclif = await import('@oclif/core')
   await oclif.execute({development: true, dir: __dirname})
